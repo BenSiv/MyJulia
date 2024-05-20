63827664c7342890395f6df94bbb0251cfabbb27
bensiv <bensiv92@gmail.com>
2024-05-20 10:50:17 +0300

    commenting activate_local_environment

--- src/MyJulia.jl
+++ src/MyJulia.jl
@@ -4,7 +4,7 @@ configurations for julia to make it more my taste
 module MyJulia
 
 import Base: +, *
-import Pkg
+# import Pkg: activate
 
 export +, *
 export activate_local_environment
@@ -12,21 +12,21 @@ export activate_local_environment
 +(a::Union{Char, String}, b::Union{Char, String}) = *(a::Union{Char, String}, b::Union{Char, String})
 *(a::Union{Char, String}, b::Int) = repeat(a::Union{Char, String}, b::Int)
 
-function activate_local_environment(current_dir)
-    while true
-        if isfile(joinpath(current_dir, "Project.toml"))
-            Pkg.activate(current_dir)
-            return
-        end
+# function activate_local_environment(current_dir)
+#     while true
+#         if isfile(joinpath(current_dir, "Project.toml"))
+#             activate(current_dir)
+#             return
+#         end
 
-        parent_dir = normpath(dirname(current_dir))
-        if parent_dir == current_dir
-            break
-        end
+#         parent_dir = normpath(dirname(current_dir))
+#         if parent_dir == current_dir
+#             break
+#         end
 
-        current_dir = parent_dir
-    end
-    println("No Project.toml found in any parent directory.")
-end
+#         current_dir = parent_dir
+#     end
+#     println("No Project.toml found in any parent directory.")
+# end
 
 end # module MyJulia
